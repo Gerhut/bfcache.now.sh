29a05f7fcbbabe7bd620c505de9e44beffce17e6
George Cheng <Gerhut@GMail.com>
2020-05-13 08:57:19 +0800

    Cache-Control: no-store

--- api/not-cached.css.ts
+++ api/not-cached.css.ts
@@ -3,7 +3,7 @@ import { IncomingMessage, ServerResponse } from 'http'
 export default (request: IncomingMessage, response: ServerResponse) => {
   response.writeHead(200, {
     'Content-Type': 'text/css; charset=utf-8',
-    'Cache-Control': 'no-cache, no-store, must-revalidate'
+    'Cache-Control': 'no-store'
   })
   response.end(`#css-not-cached:before { content: "${new Date().toISOString()}"; }`, 'utf8')
 }
--- api/not-cached.js.ts
+++ api/not-cached.js.ts
@@ -3,7 +3,7 @@ import { IncomingMessage, ServerResponse } from 'http'
 export default (request: IncomingMessage, response: ServerResponse) => {
   response.writeHead(200, {
     'Content-Type': 'text/css; charset=utf-8',
-    'Cache-Control': 'no-cache, no-store, must-revalidate'
+    'Cache-Control': 'no-store'
   })
   response.end(`document.querySelector('#js-not-cached').textContent = "${new Date().toISOString()}";`, 'utf8')
 }
